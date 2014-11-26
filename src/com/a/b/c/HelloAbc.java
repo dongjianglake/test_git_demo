62526bd259c7c331e3495d9e7f64c377f9e1ecac
lw <dongjianglake@163.com>
2014-11-26 21:31:04 +0800

    啊111111回到沙发大厦分回答说

---
+++ src/com/a/b/c/HelloAbc.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.a.b.c;
+
+/**
+ * @author jywelllee
+ *
+ */
+public class HelloAbc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+		System.out.println("fdwa回答说");
+	}
+
+}
